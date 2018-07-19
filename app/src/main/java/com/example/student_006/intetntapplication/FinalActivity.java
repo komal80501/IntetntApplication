f729a892cc76184d0e4536a4bf4ca8e14e5e57d1
komal mane <komalmane48@gmail.com>
2018-07-19 21:28:20 +0530

    Intent with Enable Navigation to app

---
+++ app/src/main/java/com/example/student_006/intetntapplication/FinalActivity.java
@@ -0,0 +1,13 @@
+package com.example.student_006.intetntapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final);
+    }
+}
--- app/src/main/java/com/example/student_006/intetntapplication/MessageActivity.java
+++ app/src/main/java/com/example/student_006/intetntapplication/MessageActivity.java
@@ -3,6 +3,7 @@ package com.example.student_006.intetntapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
@@ -11,13 +12,18 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
-        Intent intent=getIntent();
+        Intent intent = getIntent();
 
-        String message=intent.getStringExtra("Message");
+        String message = intent.getStringExtra("Message");
 
-        TextView textView=(TextView) findViewById(R.id.textViewDisplayMessage);
+        TextView textView = (TextView) findViewById(R.id.textViewDisplayMessage);
 
         textView.setText(message);
 
+    }
+
+    public void openFinal(View view) {
+
+        startActivity( new Intent(MessageActivity.this,FinalActivity.class));
     }
 }
